eeb4611969263c62d41dc7ec05e302b1a4fe939a
Matthew Collison <nmc91@15dcompd001.ncl.ac.uk>
2018-08-10 17:55:06 +0100

    updated quizzes.php.

--- header.php
+++ header.php
@@ -12,8 +12,8 @@
   <!-- JQuery 3.3.1 -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
-  <!-- popper -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+  <!-- fontawesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
 
   <!-- Custom CSS styling -->
   <link rel="stylesheet" href="../CSS/cia.css"></link>
--- quizzes.php
+++ quizzes.php
@@ -12,6 +12,8 @@ include 'navbar.php';
   </div>
 </div>
 
+<div class="container">
+  <div class="row">
 
 <?php
 //GET ALL QUIZZES FROM THE DATABASE
@@ -21,27 +23,16 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["QuizID"]. " - Name: " . $row["QuizTitle"]. " " . "<br>";
+      echo "<div class=\"col-sm-4\">"
+      echo "<a href=\"content.html\"> <i class=\"fas fa-database\"></i> </a>" //update this for relevant icon once db scema is updated to store icon link
+      echo "<p>" .  $row["QuizTitle"] . " <br \>";
+      echo "</div>"
     }
 } else {
-    echo "0 results";
+    echo "There are currently no quizzes. ";
 }
 ?>
 
-<div class="container">
-  <div class="row">
-    <div class="col-sm-4">
-      <a href="content.html"> <img src="../Images/content.png" alt="content"> </a>
-      <p>Your course materials</p>
-    </div>
-    <div class="col-sm-4">
-      <a href="quizzes.html"> <img src="../Images/Quizzes.png" alt="quizzes"> </a>
-      <p>A selection of quizzes</p>
-    </div>
-    <div class="col-sm-4">
-  	   <a href="results.html"> <img src="../Images/Results.png" alt="results"> </a>
-       <p>See your results here </p>
-    </div>
   </div>
 </div>
 
