ea2d5cfac233775822c5bf5f8ac74879db0377c7
Matthew Collison <nmc91@15dcompd001.ncl.ac.uk>
2018-08-10 18:49:00 +0100

    Added request query structure for loading quizzes.

---
+++ quizzes-summary.php
@@ -0,0 +1,31 @@
+
+<div id="header">
+  <div class="jumbotron text-center">
+    <h1>Quizzes</h1>
+    <p>Test yourself across the curriculum to identify your strengths and weaknesses</p>
+  </div>
+</div>
+
+<div class="container">
+  <div class="row">
+
+<?php
+//GET ALL QUIZZES FROM THE DATABASE
+$sql = "SELECT * FROM quizzes";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      echo "<div class=\"col-sm-4\">";
+      echo "<a href=\"?quiz=" . $row["QuizID"] . "\"> <i class=\"fa fa-question-circle fa-5x\"></i> </a>";
+      echo "<p>" .  $row["QuizTitle"] . " <br \>";
+      echo "</div>";
+    }
+} else {
+    echo "There are currently no quizzes. ";
+}
+?>
+
+  </div>
+</div>
