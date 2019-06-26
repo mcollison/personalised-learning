ebaa7d2d1b54639522fbf453b3e72b8b3a0b40d4
Matthew Collison <matthew.collison@newcastle.ac.uk>
2019-06-26 10:44:50 +0100

    added prescription images

--- connect.php
+++ connect.php
@@ -2,9 +2,9 @@
 //session_start(); //Starts PhP Session
 //* Database credentials.*/
 define('DB_SERVER', 'homepages.cs.ncl.ac.uk');
-define('DB_USERNAME', 'b5038039');
-define('DB_PASSWORD', 'Doze=IreAnts');
-define('DB_NAME', 'b5038039');
+define('DB_USERNAME', 'username');//username needs to be updated
+define('DB_PASSWORD', 'password');//password needs to be updated
+define('DB_NAME', 'dbname');//database name needs to be updated
 
 /* Attempt to connect to MySQL database */
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
